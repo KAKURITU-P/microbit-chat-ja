8868ebda62ff9956c24c71a5780b73cdca9d654f
確率P <158581461+KAKURITU-P@users.noreply.github.com>
2024-02-02 22:28:38 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> このページを開く [https://kakuritu-p.github.io/ver1-08-microbit-chat/](https://kakuritu-p.github.io/ver1-08-microbit-chat/)
+
+## 拡張機能として使用
+
+このリポジトリは、MakeCode で **拡張機能** として追加できます。
+
+* [https://makecode.microbit.org/](https://makecode.microbit.org/) を開く
+* **新しいプロジェクト** をクリックしてください
+* ギアボタンメニューの中にある **拡張機能** をクリックしてください
+* **https://github.com/kakuritu-p/ver1-08-microbit-chat** を検索してインポートします。
+
+## このプロジェクトを編集します
+
+MakeCode でこのリポジトリを編集します。
+
+* [https://makecode.microbit.org/](https://makecode.microbit.org/) を開く
+* **読み込む** をクリックし、 **URLから読み込む...** をクリックしてください
+* **https://github.com/kakuritu-p/ver1-08-microbit-chat** を貼り付けてインポートをクリックしてください
+
+#### メタデータ (検索、レンダリングに使用)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,175 @@
+enum RadioMessage {
+    message1 = 49434
+}
+// 無線帯1001
+input.onPinPressed(TouchPin.P0, function () {
+    radio.setGroup(1001)
+    basic.showLeds(`
+        # . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        `)
+})
+// メッセージリカバリー
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    送信用文字 = 送信用文字_送信時バックアップ
+    カナ文字_メモリ = カナ文字_送信時バックアップ
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (傾き == 1) {
+        basic.showArrow(ArrowNames.East)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+    傾き = 0
+    katakana.showString("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".substr(文字セレクター, 1))
+})
+// 文字選択
+input.onButtonPressed(Button.A, function () {
+    if (傾き == 0) {
+        basic.clearScreen()
+        if (文字セレクター == 62) {
+            文字セレクター = 0
+        } else {
+            文字セレクター += 1
+        }
+        katakana.showString("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".substr(文字セレクター, 1))
+    } else {
+        basic.clearScreen()
+        if (文字セレクター == -1) {
+            文字セレクター = 61
+        } else {
+            文字セレクター += -1
+        }
+        katakana.showString("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".substr(文字セレクター, 1))
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    if (傾き == 0) {
+        basic.showArrow(ArrowNames.West)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+    傾き = 1
+    katakana.showString("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".substr(文字セレクター, 1))
+})
+// 無線帯1005
+input.onPinPressed(TouchPin.P2, function () {
+    radio.setGroup(1005)
+    basic.showLeds(`
+        . . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . #
+        `)
+})
+// 取り消し(試験的)
+input.onGesture(Gesture.Shake, function () {
+    if (1 == 試験的) {
+        送信用文字 = 送信用文字_取り消し時バックアップ
+        カナ文字 = カナ文字_取り消し時バックアップ
+        送信用文字_メモリ = 送信用文字
+        カナ文字_メモリ = カナ文字
+        送信用文字 = 送信用文字_メモリ
+        カナ文字 = カナ文字_メモリ
+        basic.clearScreen()
+        katakana.setScrollTime(50)
+        katakana.showString("ﾄﾘｹｼ")
+        katakana.setScrollTime(100)
+        basic.clearScreen()
+        katakana.showString("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".substr(文字セレクター, 1))
+    } else {
+        if ("!?" == カナ文字_メモリ) {
+            試験的 = 1
+            katakana.setScrollTime(50)
+            katakana.showString("ﾄﾘｹｼ ｵﾝ")
+            katakana.setScrollTime(100)
+        }
+    }
+})
+// メッセージ送信
+// メッセージバックアップ
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString(送信用文字)
+    カナ文字_送信時バックアップ = カナ文字_メモリ
+    送信用文字_送信時バックアップ = 送信用文字
+    カナ文字 = ""
+    カナ文字_メモリ = ""
+    送信用文字 = ""
+})
+// メッセージ受信
+// メッセージ複号化
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    カナ文字_受信時メモリ = ""
+    カナ文字_受信時 = ""
+    受信文字数カウンター = 0
+    while (receivedString.length > 受信文字数カウンター) {
+        復号化用カウンター = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!".indexOf(receivedString.charAt(受信文字数カウンター))
+        カナ文字_受信時 = "" + カナ文字_受信時メモリ + "ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".charAt(復号化用カウンター)
+        カナ文字_受信時メモリ = カナ文字_受信時
+        受信文字数カウンター += 1
+    }
+    basic.pause(200)
+    basic.clearScreen()
+    katakana.showString(カナ文字_受信時)
+    basic.pause(1000)
+    basic.clearScreen()
+    katakana.showString("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".substr(文字セレクター, 1))
+})
+// メッセージ保存
+// メッセージ暗号化
+input.onButtonPressed(Button.B, function () {
+    送信用文字_メモリ = "" + 送信用文字 + "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!".substr(文字セレクター, 1)
+    カナ文字_メモリ = "" + カナ文字 + "ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".substr(文字セレクター, 1)
+    送信用文字_取り消し時バックアップ = 送信用文字
+    カナ文字_取り消し時バックアップ = カナ文字
+    送信用文字 = 送信用文字_メモリ
+    カナ文字 = カナ文字_メモリ
+})
+// 無線帯1003
+input.onPinPressed(TouchPin.P1, function () {
+    radio.setGroup(1003)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        `)
+})
+// 入力文字確認
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.clearScreen()
+    katakana.showString(カナ文字_メモリ)
+    basic.clearScreen()
+    katakana.showString("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".substr(文字セレクター, 1))
+})
+// 初期設定(リセット)
+let 復号化用カウンター = 0
+let 受信文字数カウンター = 0
+let カナ文字_受信時 = ""
+let カナ文字_受信時メモリ = ""
+let 送信用文字_メモリ = ""
+let カナ文字_取り消し時バックアップ = ""
+let カナ文字 = ""
+let 送信用文字_取り消し時バックアップ = ""
+let 傾き = 0
+let カナ文字_送信時バックアップ = ""
+let カナ文字_メモリ = ""
+let 送信用文字_送信時バックアップ = ""
+let 送信用文字 = ""
+let 文字セレクター = 0
+let 試験的 = 0
+katakana.setScrollTime(100)
+試験的 = 0
+radio.setGroup(1001)
+文字セレクター = 0
+katakana.showString("ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝｯｬｭｮｧｨｩｪｫ!?_-｡､ﾞﾟ".substr(文字セレクター, 1))
+pins.touchSetMode(TouchTarget.P0, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P1, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P2, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.LOGO, TouchTargetMode.Capacitive)
